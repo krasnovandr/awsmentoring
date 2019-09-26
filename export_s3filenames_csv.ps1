878ba26abab3867de944ca63b3653ba85780f099
Andrei Krasnou <Andrei_Krasnou@epam.com>
2019-09-26 16:48:39 +0300

    update ps script for csv export

--- export_s3filenames_csv.ps1
+++ export_s3filenames_csv.ps1
@@ -1,8 +1,22 @@
 ﻿
-$objectKeys = aws s3api list-objects --bucket andreikstaticwebsite --output text  --query 'Contents[].{Key: Key}' --profile aws_mentoring_mentee 
-$mySplit = $objectKeys -split " "
-$obj_list = $mySplit | Select-Object @{Name='Name';Expression={$_}}
-$obj_list | Export-Csv 'megaexport.csv' -NoType
+$s3objectKeys = aws s3api list-objects --bucket andreikstaticwebsite --output text  --query 'Contents[].{Key: Key}' --profile aws_mentoring_mentee 
+
+$splittedObjectKeys = $s3objectKeys -split " "
+$obj_list = $splittedObjectKeys | Select-Object @{Name='Name';Expression={$_}}
 
 
+$resultList = New-Object System.Collections.ArrayList
+
+ForEach ($item in $obj_list) 
+{
+    $fileInfo = New-Object System.Object
+    $fileName = [System.IO.Path]::GetFileName($item.Name) 
+    $fileInfo | Add-Member -MemberType NoteProperty -Name "FileName" -Value $fileName
+    $fileInfo | Add-Member -MemberType NoteProperty -Name "FilePath" -Value $item.Name
+
+    $resultList.Add($fileInfo) | Out-Null
+}
+
+
+$resultList | Export-Csv 'megaexdasdsaport.csv' -NoType
 
